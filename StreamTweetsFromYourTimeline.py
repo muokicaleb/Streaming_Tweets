18655b320e623351d918f7ffbd84509f2d59c738
muokicaleb <muokicaleb@gmail.com>
2018-08-22 11:17:19 +0300

    removed unnecessary parts

--- StreamTweetsFromYourTimeline.py
+++ StreamTweetsFromYourTimeline.py
@@ -1,9 +1,14 @@
 from tweepy import API
 from tweepy import Cursor
 from tweepy import OAuthHandler
-from tweepy import Stream
-from tweepy import StreamListener
 import twitter_credentials
+
+
+class TwitterAuthenticator():
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
 
 
 class TwitterClient():
@@ -32,65 +37,6 @@ class TwitterClient():
         return home_timeline_tweets
 
 
-# # # # TWITTER AUTHENTICATER # # # #
-class TwitterAuthenticator():
-    def authenticate_twitter_app(self):
-        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-        return auth
-
-
-# # # # TWITTER STREAMER # # # #
-class TwitterStreamer():
-    """
-    Class for streaming and processing live tweets.
-    """
-
-    def __init__(self):
-        self.twitter_autenticator = TwitterAuthenticator()
-
-    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
-        # This handles Twitter authetification and the connection to Twitter Streaming API
-        listener = TwitterListener(fetched_tweets_filename)
-        auth = self.twitter_autenticator.authenticate_twitter_app()
-        stream = Stream(auth, listener)
-        # This line filter Twitter Streams to capture data by the keywords:
-        stream.filter(track=hash_tag_list)
-
-
-# # # # TWITTER STREAM LISTENER # # # #
-class TwitterListener(StreamListener):
-    """
-    This is a basic listener that just prints received tweets to stdout.
-    """
-
-    def __init__(self, fetched_tweets_filename):
-        self.fetched_tweets_filename = fetched_tweets_filename
-
-    def on_data(self, data):
-        try:
-            print(data)
-            with open(self.fetched_tweets_filename, 'a') as tf:
-                tf.write(data)
-            return True
-        except BaseException as e:
-            print("Error on_data %s" % str(e))
-        return True
-
-    def on_error(self, status):
-        if status == 420:
-            # Returning False on_data method in case rate limit occurs.
-            return False
-        print(status)
-
-
 if __name__ == '__main__':
-    # Authenticate using config.py and connect to Twitter Streaming API.
-    hash_tag_list = ["Nairobi"]
-    fetched_tweets_filename = "tweets.txt"
-
-    twitter_client = TwitterClient('pycon')
+    twitter_client = TwitterClient('muoki_caleb')
     print(twitter_client.get_user_timeline_tweets(1))
-
-#    twitter_streamer = TwitterStreamer()
-#    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
