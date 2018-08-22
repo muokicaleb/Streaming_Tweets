19efd19c30a12541d57282aa17614ac4acebf03a
muokicaleb <muokicaleb@gmail.com>
2018-08-22 10:03:14 +0300

    completed code

--- StreamTweetsFromYourTimeline.py
+++ StreamTweetsFromYourTimeline.py
@@ -7,17 +7,32 @@ import twitter_credentials
 
 
 class TwitterClient():
-    def __init__(self):
+    def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
 
-    def get_tweets(self, num_tweets):
+        self.twitter_user = twitter_user
+
+    def get_user_timeline_tweets(self, num_tweets):
         tweets = []
-        for tweet in Cursor(self.twitter_client.user_timelime).items(num_tweets):
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
             tweets.append(tweet)
         return tweets
 
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
 
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+
+# # # # TWITTER AUTHENTICATER # # # #
 class TwitterAuthenticator():
     def authenticate_twitter_app(self):
         auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
@@ -25,26 +40,32 @@ class TwitterAuthenticator():
         return auth
 
 
+# # # # TWITTER STREAMER # # # #
 class TwitterStreamer():
+    """
+    Class for streaming and processing live tweets.
+    """
+
     def __init__(self):
-        self.twitter_authenticator = TwitterAuthenticator()
+        self.twitter_autenticator = TwitterAuthenticator()
 
     def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter authetification and the connection to Twitter Streaming API
         listener = TwitterListener(fetched_tweets_filename)
-        auth = self.twitter_authenticator.authenticate_twitter_app()
+        auth = self.twitter_autenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
+        # This line filter Twitter Streams to capture data by the keywords:
         stream.filter(track=hash_tag_list)
 
 
+# # # # TWITTER STREAM LISTENER # # # #
 class TwitterListener(StreamListener):
+    """
+    This is a basic listener that just prints received tweets to stdout.
+    """
+
     def __init__(self, fetched_tweets_filename):
         self.fetched_tweets_filename = fetched_tweets_filename
-
-    '''
-       This method shows the data that has been streamed
-       In our case it prints the data on the screen and also
-       writes  it in a file
-    '''
 
     def on_data(self, data):
         try:
@@ -52,26 +73,24 @@ class TwitterListener(StreamListener):
             with open(self.fetched_tweets_filename, 'a') as tf:
                 tf.write(data)
             return True
-
         except BaseException as e:
             print("Error on_data %s" % str(e))
         return True
 
-    '''
-        In the event of an error it prints the error
-    '''
-
     def on_error(self, status):
         if status == 420:
+            # Returning False on_data method in case rate limit occurs.
             return False
         print(status)
 
 
 if __name__ == '__main__':
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    hash_tag_list = ["donal trump", "hillary clinton", "barack obama", "bernie sanders"]
+    fetched_tweets_filename = "tweets.txt"
 
-    hash_tag_list = ['KenyanTraffic', 'Nairobi', 'Thika']
-    fetched_tweets_filename = 'tweets.txt'
-    twitter_client = TwitterClient()
-    print(twitter_client.get_user_timeline_tweets(2))
+    twitter_client = TwitterClient('pycon')
+    print(twitter_client.get_user_timeline_tweets(1))
+
 #    twitter_streamer = TwitterStreamer()
 #    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
