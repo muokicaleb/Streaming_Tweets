f55c10ea638750990d5414a5ffef8a69a3897edd
muokicaleb <muokicaleb@gmail.com>
2018-08-23 21:18:44 +0300

    commit before branching

--- analyzingTweetData.py
+++ analyzingTweetData.py
@@ -17,26 +17,10 @@ class TwitterClient():
     def __init__(self, twitter_user=None):
         self.auth = TwitterAuthenticator().authenticate_twitter_app()
         self.twitter_client = API(self.auth)
-
         self.twitter_user = twitter_user
 
-    def get_user_timeline_tweets(self, num_tweets):
-        tweets = []
-        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
-            tweets.append(tweet)
-        return tweets
-
-    def get_friend_list(self, num_friends):
-        friend_list = []
-        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
-            friend_list.append(friend)
-        return friend_list
-
-    def get_home_timeline_tweets(self, num_tweets):
-        home_timeline_tweets = []
-        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
-            home_timeline_tweets.append(tweet)
-        return home_timeline_tweets
+    def get_twitter_client_api(self):
+        return self.twitter_client
 
 
 class TweetAnalyzer():
@@ -46,7 +30,6 @@ class TweetAnalyzer():
 
     def tweets_to_data_frame(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
-
         df['id'] = np.array([tweet.id for tweet in tweets])
         df['len'] = np.array([len(tweet.text) for tweet in tweets])
         df['date'] = np.array([tweet.created_at for tweet in tweets])
@@ -64,11 +47,8 @@ if __name__ == '__main__':
 
     api = twitter_client.get_twitter_client_api()
 
-    tweets = api.user_timeline(screen_name="realDonaldTrump", count=20)
-
-    # print(dir(tweets[0]))
-    # print(tweets[0].retweet_count)
+    tweets = api.user_timeline(screen_name="muoki_caleb", count=20)
 
     df = tweet_analyzer.tweets_to_data_frame(tweets)
 
-    print(df.head(10))
+    print(df.head(20))
