8741e8d22730cbf36a2c6fd795d1aa41d3629dfb
muokicaleb <muokicaleb@gmail.com>
2018-08-24 23:54:42 +0300

    Revert "Merge branch 'allfollowers'"
    This reverts commit ac7c989d344a2f59b60d5d21d1271869a4c5553c, reversing
    changes made to 4e4658a33e08d8e8f0efe5bb0bf974e597b517b1.

---
+++ StreamTweetsFromYourTimeline.py
@@ -0,0 +1,43 @@
+from tweepy import API
+from tweepy import Cursor
+from tweepy import OAuthHandler
+import twitter_credentials
+
+
+class TwitterAuthenticator():
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
+
+
+class TwitterClient():
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+
+        self.twitter_user = twitter_user
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_friend_list(self, num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friend_list.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self, num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+
+if __name__ == '__main__':
+    twitter_client = TwitterClient('muoki_caleb')
+    print(twitter_client.get_user_timeline_tweets(1))
+    # print(twitter_client.get_friend_list(2))
